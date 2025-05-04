cb179d19cf30256b120888272405c8aa41c3b947
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 01:16:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.port || 3000
 
 
-app.use(cors(allowedOrigins));
+app.use(cors());
 
 
 app.use(express.json());
@@ -16,11 +16,9 @@ app.use("/", PostRouter);
 
 
 app.use((req, res, next) => {
-    const allowedOrigins = ["http://localhost:5173"];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader({'Access-Control-Allow-Origin': "http://localhost:5173"});
-    }
+   
+  
+    res.setHeader({'Access-Control-Allow-Origin': "*"});
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
