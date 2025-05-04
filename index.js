f2e63fffaa7ca0775886e5c9c0d8effd20100093
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-04 23:58:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ import PostRouter from "./src/modules/posts/post.routes.js"
 
 const app = express()
 
-app.use(cors({origin:"*"}));
 
 
-
-
-
-
-
-
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", userRouter); 
 app.use("/", PostRouter); 
+
+
+
+
+var whitelist = ['*']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 
 
