e4b1b4160d14e8304b4294e7bf8721d86b54d704
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 01:18:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const app = express();
 const port = process.env.port || 3000
 
 
-app.use(cors());
+app.use((req, res, next) => {
+  cors({origin :"*"})
+  res.setHeader({'Access-Control-Allow-Origin': "*"});
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 app.use(express.json());
@@ -15,15 +22,7 @@ app.use("/", userRouter);
 app.use("/", PostRouter); 
 
 
-app.use((req, res, next) => {
-   
-  
-    res.setHeader({'Access-Control-Allow-Origin': "*"});
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+
 
 
 
