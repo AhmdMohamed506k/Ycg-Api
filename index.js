b3e440de33341290edcab5e3d3fcd5130b1ee4fb
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 00:55:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,25 @@ const app = express();
 const port = process.env.port || 3000
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use(express.json());
 app.use("/", userRouter); 
 app.use("/", PostRouter); 
 
-var whitelist = ["http://localhost:5173",'http://127.0.0.1:5500',"127.0.0.1:27017"]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+
+app.use((req,res,next)=>{
+  const allowedOrigins = ['*'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
-}
-
-
-
-
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
 
 
 
