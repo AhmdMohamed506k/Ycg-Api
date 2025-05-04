6e3d5675149a8bb8e55276abed1382a06b915345
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 00:25:10 +0300

    Update post.routes.js

--- src/modules/posts/post.routes.js
+++ src/modules/posts/post.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as p from "./post.controller.js"
 import { auth } from "../../middleware/auth.js";
+import cors from "cors"
 
 
 
@@ -13,9 +14,9 @@ const router = Router();
 router.get("/getPosts",p.getPosts);
 router.get("/getPostsByJobFeild/:jobFeild",p.GetPostsByJobFeild);
 router.get("/getPostsbyId/:id",p.GetPostsById);
-router.post("/addPosts",auth, p.addpost);
-router.put("/updatePost",auth , p.updatePost);
-router.delete("/deletPost",auth , p.deletPost);
+router.post("/addPosts",auth, cors(), p.addpost);
+router.put("/updatePost",auth ,cors(), p.updatePost);
+router.delete("/deletPost",auth , cors(), p.deletPost);
 
 
 
