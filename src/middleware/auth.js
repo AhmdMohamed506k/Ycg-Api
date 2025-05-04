38fbcc7d29902d88be367649f722763eb4b1737e
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-04 23:51:41 +0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -21,10 +21,8 @@ export const auth = async (req, res, next) => {
       return res.status(400).json({ msg: "you are not authorised" });
     }
     res.header({"Access-Control-Allow-Origin":"*"})
-    res.header({'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token'})
- 
-    res.header({"Access-Control-Allow-Methods":"GET,POST,PUT,DELETE,OPTIONS"})
-    res.header({'Access-Control-Allow-Credentails': true})
+   
+
     
     req.user = user
     next()
