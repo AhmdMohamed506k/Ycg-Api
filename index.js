c26f92c481e308e9889c709d4a93bb4b7db774dc
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 00:01:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import connectionDB from './DB/contectionDB.js';
 import cors from 'cors';
 import userRouter from "./src/modules/users/user.routes.js"
 import PostRouter from "./src/modules/posts/post.routes.js"
-
-
 const app = express()
+const port = process.env.port || 3000
+
 
 
 
