9df3db8cd7241037374595762c99b074a01e3f81
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 00:38:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ import userRouter from "./src/modules/users/user.routes.js"
 import PostRouter from "./src/modules/posts/post.routes.js"
 const app = express();
 const port = process.env.port || 3000
-connectionDB()
-
-
 
 
 
 app.use(express.json());
+
+
+
+
+
+app.use(cors());
 app.use("/", userRouter); 
 app.use("/", PostRouter); 
 
 
-app.use(cors());
 
 
 
@@ -24,5 +26,6 @@ app.use(cors());
 
 
 
+connectionDB()
 app.use('/', (req, res) => res.send('Hello World hi!'))
 app.listen(port, () => console.log(`successfully connected`))
