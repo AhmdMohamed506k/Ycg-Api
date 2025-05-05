df2e1a2b5959b56c7b8c3dc502d4a72bb01ae530
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 20:20:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ const port = process.env.port || 3000
 
 app.use(express.json());
 
-app.use(cors(whitelist))
+app.use(cors(corsOptions))
 
 var whitelist = ['http://localhost:5173','http://localhost:5173/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+app.use((req,res,next)=>{
+  var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
     }
   }
-}
 
+  
+})
 
 
 
