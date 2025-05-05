c726c396b382d82b4904fc2b3a17005e78203957
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 22:07:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ export const app = express()
 
 const port = process.env.port || 3000
 
-app.use(cors(corsOptions));
-
-
-var whitelist = ["http://localhost:5173"]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+app.use(cors())
 
 app.use(express.json());
 app.use("/", userRouter); 
