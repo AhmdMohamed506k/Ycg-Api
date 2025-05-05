265a00e0857c350ff9577608eb96bf28e895f6f6
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 22:18:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ export const app = express()
 
 const port = process.env.port || 3000
 
-app.use(cors())
+
 
 app.use(express.json());
+
+app.use(cors())
 app.use("/", userRouter); 
 app.use("/", PostRouter); 
 
