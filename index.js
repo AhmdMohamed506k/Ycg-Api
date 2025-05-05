2631237bc9ba9ed6611f7c60de3ba3a3ca972dce
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 20:23:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,17 @@ app.use(express.json());
 app.use(cors(corsOptions))
 
 var whitelist = ['http://localhost:5173','https://ycg-ecru.vercel.app']
-app.use((req,res,next)=>{
-  var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+      Headers('Access-Control-Allow-Origin', origin);
+   
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
   }
-
-  
-})
+}
 
 
 
