efcdd0be495fcabb6130e2c18849de2d2547ad77
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 22:35:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ import connectionDB from './DB/contectionDB.js';
 import cors from 'cors';
 import userRouter from "./src/modules/users/user.routes.js"
 import PostRouter from "./src/modules/posts/post.routes.js"
-
-
 export const app = express()
-
 const port = process.env.port || 3000
 
 
+app.use(cors())
 
 app.use(express.json());
 
-app.use(cors())
+
 app.use("/", userRouter); 
 app.use("/", PostRouter); 
 
