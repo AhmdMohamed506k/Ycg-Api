a79604735e8309e5f6fc6e600c0838a507c8122f
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 20:44:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,7 @@ const port = process.env.port || 3000
 app.use(express.json());
 
 
-app.use(function (req, res, next)  {
-  const allowedOrigins = ['*'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader({'Access-Control-Allow-Origin' : origin});
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors())
 
 
 
