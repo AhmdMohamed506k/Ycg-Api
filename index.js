403e5f0353dbc316ba70c36ebb111b2143873e75
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2025-05-05 18:44:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 const port = process.env.port || 3000
 
 
-app.use(express.json());
+
 app.use(cors())
 
-
+app.use(express.json());
 
 app.use(function (req, res, next)  {
  
